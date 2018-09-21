81d7b2f488f2616f41c6162fe59fe46c46f49e97
Sai Kumbhoji <saikrishnak@vs-macmini1.local>
2018-09-21 10:15:24 +0530

    Second line has been added.

--- Test/AppDelegate.swift
+++ Test/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         print("First Line has been added by Saikrishna")
+        print("Second Line added by Saikrishna")
         return true
     }
 
